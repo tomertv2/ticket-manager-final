c297ebd0b074e9c72b2e332e38013c4f84b18f08
tomertv2 <tomertv2@gmail.com>
2020-08-24 13:47:23 +0300

    GET all tickets

--- server/app.js
+++ server/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const fs = require('fs').promises;
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/tickets', async (req, res) => {
+  const tickets = await fs.readFile('./data.json');
+  const json = JSON.parse(tickets);
+  res.send(json);
+});
 
 module.exports = app;
